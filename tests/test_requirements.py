3ab46fdaa23c93869e140af0cd06bb68168251f2
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2020-12-10 12:12:47 +0100

    test(requirements): add test to verify requirements in virtual environment

---
+++ tests/test_requirements.py
@@ -0,0 +1,23 @@
+# =============================================================================
+# C O P Y R I G H T
+# -----------------------------------------------------------------------------
+# Copyright (c) 2020 by Helmut Konrad Fahrendholz. All rights reserved.
+# This file is property of Helmut Konrad Fahrendholz. Any unauthorized copy,
+# use or distribution is an offensive act against international law and may
+# be prosecuted under federal law. Its content is company confidential.
+# =============================================================================
+
+import genex
+import power
+
+import genex_reference
+
+
+def test_generator_run(testdir):
+    power.setup(genex_reference.ROOT)
+    files = [power.TECH019_PDF, power.REPOSITORY]
+    genex.extract(
+        files=files,
+        destination=testdir.tmpdir,
+        full=True,
+    )
