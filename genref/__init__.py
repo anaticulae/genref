6b76b43d6542151dce5e45b4965549daf69667d6
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2021-02-05 13:52:29 +0100

    1.0.2 auto generated release

--- genref/__init__.py
+++ genref/__init__.py
@@ -8,6 +8,6 @@
 #==============================================================================
 import os
 
-__version__ = '1.0.1'
+__version__ = '1.0.2'
 
 ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
